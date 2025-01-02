ef3c3efad9a35c332a42d72db6a92fd7997a5dcf
Hash-Cracker <tspamiitesh@gmail.com>
2025-01-02 19:56:05 +0530

    Writing the things to do in comment

--- view.c
+++ view.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#include <stdio.h>  // #88C0D0 for function names, #4C566A for comments
 #include <stdlib.h>
 
-void print_file(const char *filename) {
+void print_file(const char *filename) {  // #81A1C1 for keywords, #8FBCBB for constants
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         perror("Error opening file");
         return;
     }
 
-    char buffer[1024];
+    char buffer[1024];  // #A3BE8C for strings
     size_t bytesRead;
 
     while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0) {
@@ -19,7 +19,7 @@ void print_file(const char *filename) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
+    if (argc < 2) {  // #81A1C1 for keywords, #BF616A for errors
         char buffer[1024];
         while (fgets(buffer, sizeof(buffer), stdin)) {
             fputs(buffer, stdout);
